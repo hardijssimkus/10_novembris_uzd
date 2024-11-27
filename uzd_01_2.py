670a8b193f453cfa10cc6aa43bcc2d483690ef87
hardijssimkus <hardijssimkus@grobinasskola.lv>
2024-11-27 14:21:19 +0200

    uzd_01_2

--- uzd_01_2.py
+++ uzd_01_2.py
@@ -2,4 +2,13 @@
 # tilpums = 3.14 * râdiuss * râdiuss * augstums
 # laukums = 2 * (3.14 * râdiuss * râdiuss) + augstums * (2 * 3.14 * râdiuss)
 
+rādiuss = float(input("Ievadiet cilindra rādiusu: "))
+augstums = float(input("Ievadiet cilindra augstumu: "))
+
+tilpums = 3.14 * rādiuss * rādiuss * augstums
+
+laukums = 2 * (3.14 * rādiuss * rādiuss) + augstums * (2 * 3.14 * rādiuss)
+
+print("Cilindra tilpums ir:", round(tilpums, 2), "kubiskie metri")
+print("Cilindra laukums ir:", round(laukums, 2), "kvadrātmetri")
 
