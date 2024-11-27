e43d3fbac3f6187ec4a36e7396babb419cb44cbc
hardijssimkus <hardijssimkus@grobinasskola.lv>
2024-11-27 14:13:55 +0200

    uzd 1

--- uzd_01_1.py
+++ uzd_01_1.py
@@ -1,3 +1,10 @@
 # Uzdevums: 01-1: Izveidot programmu, kas prasa lietotâjam ievadît skaitli n, tad programma aprçíina n+nn+nnn. 
-Rezultâts tiek parâdîts konsolç.
+#rezultāts tiek parādīts konsolē.
+
+
+n = input("Ievadiet skaitli n: ")
+nn = n * 2  
+nnn = n * 3  
+rezultāts = int(n) + int(nn) + int(nnn)
+print("Rezultāts ir:", rezultāts)
 
